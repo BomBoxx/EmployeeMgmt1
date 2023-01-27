c1dd56f0ffe850567d6426cb9576f6b3bbed8557
BomBoxx <abdelrahmanmarwan52@gmail.com>
2023-01-27 19:25:49 +0200

    Con = new SqlConnection(ConStr);

--- EmployeeMgmt1/Functions.cs
+++ EmployeeMgmt1/Functions.cs
@@ -18,7 +18,7 @@ namespace EmployeeMgmt1
         public Functions() 
         {
             ConStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\USERS\ABDELRAHMAN\DOCUMENTS\EMPDB.MDF"";Integrated Security=True;Connect Timeout=30"; 
-            Con = new SqlConnection(ConStr);
+            Con = new SqlConnection(ConStr); 
             Cmd = new SqlCommand();
             Cmd.Connection = Con;
         }
