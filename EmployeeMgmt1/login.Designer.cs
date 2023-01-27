92a57554308c63689eb16275f3e94c91478cbb3d
BomBoxx <abdelrahmanmarwan52@gmail.com>
2023-01-27 17:31:56 +0200

    adding password label

--- EmployeeMgmt1/login.Designer.cs
+++ EmployeeMgmt1/login.Designer.cs
@@ -106,7 +106,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Teal;
-            this.label3.Location = new System.Drawing.Point(375, 228);
+            this.label3.Location = new System.Drawing.Point(377, 228);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(104, 25);
             this.label3.TabIndex = 6;
